4fd27ec09d4b6d61537dee4ec6b5155aefa85d16
theSPGN <94869555+theSPGN@users.noreply.github.com>
2023-04-18 20:27:53 +0200

    Add files via upload

---
+++ zadanie_z_cwiczen_03.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+printf("\n\n==============\nMateusz Zajda\nAkademia Górniczo-Hutnicza im. Stanisława Staszica w Krakowie \nIMIR\n2021\n13:00 C1-506\nDr Hilger\n==============\n\n ");
+printf("%20s","Z poważaniem");
+printf("\n%33s%d","Mateusz Zajda APIR grupa:",9);
+printf("\n\n");
+return 0;}
